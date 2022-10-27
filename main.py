10c1f644f9889416668f0c7451cc4aabb04303c8
Sion Avakian <avakianssion@gmail.com>
2022-10-27 11:58:36 -0400

    Add Project and About pages with nav bar

--- main.py
+++ main.py
@@ -1,23 +1,77 @@
-#
 import flet
-from flet import Page, Text, Row, TextField, ElevatedButton
+from flet import AppBar, ElevatedButton, Page, Text, View, colors
 
 
-def main(page):
-    name = 'John R'
-    t = Text()
-    
-    def button_clicked(e):
-        page.add(Text(f"Hello {name}, this string was formatted"))
-    
-    page.add(
-        Row(controls = [
-            TextField(label="Name"),
-            ElevatedButton(text="Say my name!", on_click=button_clicked)
-        ])
-    )
-    page.update()
+def main(page: Page):
+    page.title = "Home"
 
- 
+    print("Initial route:", page.route)
+
+    def route_change(e):
+        print("Route change:", e.route)
+        page.views.clear()
+        page.views.append(
+            View(
+                "/",
+                [
+                    AppBar(title=Text("Personal Portfolio")),
+                    ElevatedButton("About", on_click=open_settings),
+                    ElevatedButton("Projects", on_click=open_projects),
+
+                ],
+            )
+        )
+        
+        if page.route == "/About":
+            page.views.append(
+                View(
+                    "/About",
+                    [
+                        AppBar(title=Text("About"), bgcolor=colors.SURFACE_VARIANT),
+                        Text("About Page!", style="bodyMedium"),
+                    ],
+                )
+            )
+        if page.route == "/Projects":
+            page.views.append(
+                View(
+                    "/Projects",
+                    [
+                        AppBar(title=Text("Projects"), bgcolor=colors.SURFACE_VARIANT),
+                        Text("Projects Page!", style="bodyMedium"),
+                    ],
+                )
+            )
+        # if page.route == "/settings/mail":
+        #     page.views.append(
+        #         View(
+        #             "/settings/mail",
+        #             [
+        #                 AppBar(
+        #                     title=Text("Mail Settings"), bgcolor=colors.SURFACE_VARIANT
+        #                 ),
+        #                 Text("Mail settings!"),
+        #             ],
+        #         )
+        #     )
+        page.update()
+
+    def view_pop(e):
+        print("View pop:", e.view)
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+
+    def open_projects(e):
+        page.go("/Projects")
+
+    def open_settings(e):
+        page.go("/About")
+
+    page.go(page.route)
+
 
 flet.app(target=main, view=flet.WEB_BROWSER)
