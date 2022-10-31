6cc569d93b6dc09c57f2c49478eeba4914d8a543
Sion Avakian <avakianssion@gmail.com>
2022-10-31 12:06:04 -0400

    Improved App Bar, Improved Welcome Message

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from turtle import onclick
 import flet
-from flet import AppBar, FilledTonalButton ,OutlinedButton, Page, Text, View, colors, Image, Container, Row, alignment, CircleAvatar,NavigationRail, NavigationRailDestination, FloatingActionButton, Icon, icons, VerticalDivider
+from flet import AppBar, FilledTonalButton ,OutlinedButton, Page, Text, View, colors, Image, Container, Row, Column, alignment, CircleAvatar,NavigationRail, NavigationRailDestination, FloatingActionButton, Icon, icons, VerticalDivider
+
 
 
 
@@ -7,80 +9,56 @@ def main(page: Page):
     page.title = "Home"
     
     print("Initial route:", page.route)
+    
 
     def route_change(e):
         page.fonts={"Montserrat-Light": "/Fonts/Montserrat-Light.tff",
                     "Montserrat-Bold": "/Fonts/Montserrat-Bold.ttf",
                     "Montserrat-Medium": "/Fonts/Montserrat-Medium.tff",
                     "Montserrat-Regular": "/Fonts/Montserrat-Regular.tff"}
+
         print("Route change:", e.route)
         page.views.clear()
-        rail = NavigationRail(
-                            selected_index=0,
-                            label_type="all",
-                            # extended=True,
-                            min_width=100,
-                            min_extended_width=400,
-                            height=500,
-                            leading=FloatingActionButton(icon=icons.CREATE, text="Add"),
-                            group_alignment=-0.9,
-                            destinations=[
-                                NavigationRailDestination(
-                                    icon=icons.FAVORITE_BORDER, selected_icon=icons.FAVORITE, label="First"
-                                ),
-                                NavigationRailDestination(
-                                    icon_content=Icon(icons.BOOKMARK_BORDER),
-                                    selected_icon_content=Icon(icons.BOOKMARK),
-                                    label="Second",
-                                ),
-                                NavigationRailDestination(
-                                    icon=icons.SETTINGS_OUTLINED,
-                                    selected_icon_content=Icon(icons.SETTINGS),
-                                    label_content=Text("Settings"),
-                                ),
+        #Github, Linkedin
+        contact_buttons = Row([])
+        navigation_buttons = Row(
+                            [
+                            Container(
+                                content = OutlinedButton("Projects", on_click=open_projects),
+                                alignment=alignment.center,
+                                width=120,
+                                height=20,
+                            ),
+                            Container(
+                                content=OutlinedButton("Experience", on_click=open_experience),
+                                alignment=alignment.center,
+                                width=120,
+                                height=20,
+                            ),
                             ],
-                        )
+                            alignment="center",
+                            )
         page.views.append(
             View(
                 "/",
                 [
-                    # AppBar(title=Text("Sion Avakian", size=50, font_family="Montserrat-Regular", color=colors.WHITE), center_title=True, bgcolor=colors.BLUE_GREY),
-                    Container(content=Text("Sion Avakian", size=50, font_family="Montserrat-Regular",color=colors.WHITE),alignment=alignment.center),
-                    Row(
+                    AppBar(title=Text("Sion Avakian", font_family="Montserrat-Regular"), actions=[navigation_buttons], center_title=True),
+                    # Container(content=Text("Sion Avakian", size=50, font_family="Montserrat-Regular",color=colors.WHITE),alignment=alignment.center),
+                    Column(
                         [
-                        rail,
-                        VerticalDivider(width=1),
-                        Container(
-                            content=FilledTonalButton("Contact", on_click=open_settings),
-                            alignment=alignment.center,
-                            width=150,
-                            height=150,
-                        ),
-                        Container(
-                            content = FilledTonalButton("Projects", on_click=open_projects),
-                            alignment=alignment.center,
-                            width=150,
-                            height=150,
-                        ),
-                        Container(
-                            content=FilledTonalButton("Experience", on_click=open_experience),
-                            alignment=alignment.center,
-                            width=150,
-                            height=150,
-                        ),
+                        Column(
+                            [Container(content=CircleAvatar(foreground_image_url="Images/headshot.jpg", content=Text("FF"), width=200, height=200),alignment=alignment.center,padding=20),
+                            Container(content=Text("Software Engineer and Computer Science Enthusiast", size=25, font_family="Montserrat-Light",color=colors.WHITE),alignment=alignment.center,padding=20),
+                            ],
+                            )
                         ],
-                        alignment="start",
-
-                    ),
-                    Container(content=CircleAvatar(foreground_image_url="Images/headshot.jpg", content=Text("FF"), width=200, height=200), alignment=alignment.center,padding=20),
-                    Container(content=Text("Software engineer and computer science enthusiast.", size=25, font_family="Montserrat-Light",color=colors.WHITE),alignment=alignment.center,padding=20),
-
+                    )
                 ],
             bgcolor=colors.BLUE_GREY,
             scroll="always",
-    
 
             ),
+
         )
 
         if page.route == "/About":
@@ -104,7 +82,10 @@ def main(page: Page):
                         AppBar(title=Text("Projects"), bgcolor=colors.SURFACE_VARIANT),
                         Text("Projects Page!", style="bodyMedium"),
                     ],
-                scroll="always"
+                scroll="always",
+                bgcolor=colors.YELLOW_ACCENT_200
+
+                
                 )
             )
         if page.route == "/Experience":
@@ -145,14 +126,10 @@ def main(page: Page):
     def open_projects(e):
         page.go("/Projects")
 
-    def open_settings(e):
-        page.go("/About")
     def open_experience(e):
         page.go("/Experience")
- 
 
     page.go(page.route)
-
 
 flet.app(
     target=main, view=flet.WEB_BROWSER,
